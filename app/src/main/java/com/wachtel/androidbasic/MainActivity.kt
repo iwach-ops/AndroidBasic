0cb8a7ff62a8fb409333281c95be16f03e30cd45
I Wachtel <iwachtel73@gmail.com>
2026-06-09 21:38:50 +0200

    second commit

--- app/src/main/java/com/wachtel/androidbasic/MainActivity.kt
+++ app/src/main/java/com/wachtel/androidbasic/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,10 +40,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidBasicTheme {
-        Greeting("Android")
+            Greeting("Android")
     }
 }
